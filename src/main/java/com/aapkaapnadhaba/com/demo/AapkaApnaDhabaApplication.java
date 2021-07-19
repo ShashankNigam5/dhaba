a46edc186169e1dd5c94e3ed1519bf2425722c2b
ShashankNigam5 <shashank261192@gmail.com>
2021-07-19 23:31:25 +0530

    Base setup commit

---
+++ src/main/java/com/aapkaapnadhaba/com/demo/AapkaApnaDhabaApplication.java
@@ -0,0 +1,13 @@
+package com.aapkaapnadhaba.com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AapkaApnaDhabaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AapkaApnaDhabaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aapkaapnadhaba/com/demo/AapkaApnaDhabaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aapkaapnadhaba.com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AapkaApnaDhabaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
